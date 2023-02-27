56d1721c15d6660a270ed11ff78a710555b3cc61
Adil Khan <112151943+adilk121@users.noreply.github.com>
2023-02-27 17:29:04 +0530

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
-
+//Controler
 class AuthController extends Controller
 {
     public function logout()
